4ca63faa301e60f721b2b3d238a5b18b7446b8c7
bogibekdev <bogibekdev@gmail.com>
2023-05-08 20:47:18 +0500

    project finished

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,205 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey.shade100,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //#header
+            Container(
+              height: 300,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/ic_header.jpg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomRight,
+                    colors: [
+                      Colors.black.withOpacity(.8),
+                      Colors.black.withOpacity(.4),
+                    ],
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text("What kind of hotel you need?",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center),
+                    SizedBox(height: 30),
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 3),
+                      margin: EdgeInsets.symmetric(horizontal: 40),
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.search, color: Colors.grey),
+                          hintText: "Search to hotels...",
+                          hintStyle:
+                              TextStyle(color: Colors.grey, fontSize: 18),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+
+            // #body1
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Best Hotels",
+                    style: TextStyle(
+                        color: Colors.grey.shade800,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 30),
+                  Container(
+                    height: 200,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        item(
+                            image: "assets/images/ic_hotel1.jpg",
+                            title: "BOO Hotel"),
+                        item(
+                            image: "assets/images/ic_hotel2.jpg",
+                            title: "BOO Hotel"),
+                        item(
+                            image: "assets/images/ic_hotel3.jpg",
+                            title: "BOO Hotel"),
+
+                        item(
+                            image: "assets/images/ic_hotel4.jpg",
+                            title: "BOO Hotel"),
+
+                        item(
+                            image: "assets/images/ic_hotel2.jpg",
+                            title: "BOO Hotel"),
+
+                        item(
+                            image: "assets/images/ic_hotel1.jpg",
+                            title: "BOO Hotel"),
+
+                        item(
+                            image: "assets/images/ic_hotel3.jpg",
+                            title: "BOO Hotel"),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 30),
+
+            // #body2
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Luxury Hotels",
+                    style: TextStyle(
+                        color: Colors.grey.shade800,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 30),
+                  Container(
+                    height: 200,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        item(
+                            image: "assets/images/ic_hotel3.jpg",
+                            title: "BOO Hotel"),
+                        item(
+                            image: "assets/images/ic_hotel1.jpg",
+                            title: "BOO Hotel"),
+                        item(
+                            image: "assets/images/ic_hotel2.jpg",
+                            title: "BOO Hotel"),
+
+
+                        item(
+                            image: "assets/images/ic_hotel4.jpg",
+                            title: "BOO Hotel"),
+
+                        item(
+                            image: "assets/images/ic_hotel2.jpg",
+                            title: "BOO Hotel"),
+
+                        item(
+                            image: "assets/images/ic_hotel1.jpg",
+                            title: "BOO Hotel"),
+
+                        item(
+                            image: "assets/images/ic_hotel3.jpg",
+                            title: "BOO Hotel"),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 30),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget item({image, title}) {
+    return AspectRatio(
+      aspectRatio: 1.4 / 1,
+      child: Container(
+        margin: EdgeInsets.only(right: 15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover),
+        ),
+        child: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+                begin: Alignment.bottomRight,
+                colors: [
+                  Colors.black.withOpacity(.8),
+                  Colors.black.withOpacity(.2),
+                ],
+              )),
+          child: Align(
+            alignment: Alignment.bottomLeft,
+            child: Text(
+              title,
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
